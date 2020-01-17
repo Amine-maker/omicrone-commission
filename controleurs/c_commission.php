521aced71049625bf9702d66b6530b25273dc76c
amine <amine.djennadi@hotmail.fr>
2020-01-17 12:42:38 +0100

    dfdfqe

---
+++ controleurs/c_commission.php
@@ -0,0 +1,46 @@
+<?php 
+if (!isset($_REQUEST['action'])){
+	$_REQUEST['action']=NULL;
+}
+$action = $_REQUEST['action'];
+$commerciauxDAO=new commerciauxDAO;
+$commissionDAO= new commissionDAO;
+switch($action){
+
+	case 'ajouterCommission':{
+
+		$commission=new commission($commerciauxDAO->getCommercial($_POST["idCommercial"]));
+		$commissionDAO->add($commission,$commerciauxDAO,$_POST);
+		header('location:index.php?uc=commission&action=afficherCommission');
+		break;
+	}
+
+	case 'afficherCommission':{
+		$lesCommerciaux=$commerciauxDAO->getCommerciaux();
+		$lesCommissions=$commissionDAO->getCommissions();
+		include("vues/v_tableauCommission.php");
+		break;
+	}
+	
+	case 'updateCommission': {
+		
+		$_REQUEST["heri"]=explode(",",$_REQUEST["tableau"]);
+		if($_REQUEST["heri"][0]==''){$_REQUEST["heri"][0]="Null";}
+		$commercial=$commerciauxDAO->getCommercial($_REQUEST["idCommercial"])->getOCommercial();
+		if(!stristr($_REQUEST["heri"][0],"Null")){
+			$commission=new one_shot($_REQUEST["heri"][0],$commercial);
+			$commissionDAO->update($commission,$_REQUEST["idCommission"]);
+		}
+		else{
+			$commission=new pourcentage($_REQUEST["heri"][1],$commercial);
+			$commissionDAO->update($commission,$_REQUEST["idCommission"]);
+		}
+		
+		header('location:index.php?uc=commission&action=afficherCommission');
+		break;
+	}
+	case 'deleteCommission': {
+		$commissionDAO->delete($_REQUEST["idCommission"]);
+		header('location:index.php?uc=commission&action=afficherCommission');
+	}
+}
---
+++ vues/v_facture.php
@@ -0,0 +1,28 @@
+<div class='container'>
+    <div class='form-style-5'>
+         <form method='POST' action='index.php?uc=facture&action=genererfacture'>
+            <fieldset>
+                <legend>Facture du client</legend>
+                <?php 
+                       print_r($UnContrat);
+                    ?>
+                <p>L'entreprise : <?php echo  $raisonsocial = $UnContrat->getcleclient()->getraisonsocial(); ?></p>
+                <input type='hidden' name='rsl' required='required' value='<?php echo $raisonsocial?>' placeholder='Le Client'>
+                <?php if ($UnContrat->getsalaire() == 0 ){
+                    ?><p>Le montant dû est :<?php $tarif = $UnContrat->gettarif();?></p>
+                      <input type='hidden' name='tarif' required='required' value='<?php echo $tarif?>' placeholder='Le montant dû '>
+                <?php } 
+                else { ?>
+                     <p>Le montant dû est : <?php $salaire = $UnContrat->getsalaire();?></p>
+                    <input type='hidden' name='salaire' required='required' value='<?php echo $salaire?>' placeholder='Le montant dû '>
+                <?php }?>
+                
+                
+                <p>Date de création de la facture : <?php echo date('d/m/Y');?></p>
+                <input type='hidden' name='datej' required='required' value='<?php date('Y,m,d');?>' placeholder='<?php echo date('d/m/Y');?>'>
+                <input type='submit' name='creerfacture' value='Créer la facture'>
+            </fieldset>
+         </form>
+    
+</div>
+</div>
