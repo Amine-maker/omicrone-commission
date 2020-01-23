e3dd458468bae91c137c71b8165dd403eb55f038
amine <amine.djennadi@hotmail.fr>
2020-01-23 17:23:28 +0100

    fg

--- js/recherche.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#categoryFilter').on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $('#filter .filter_td').parent().filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
-}); 
